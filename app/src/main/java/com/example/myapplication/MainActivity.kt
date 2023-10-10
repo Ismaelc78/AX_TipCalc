4c93ccf4ab9590c978fd13be0e5d773b85ce0b4f
ismael <ismaelcontreras4377@gmail.com>
2023-10-09 22:19:43 -0600

    Added Calculator Function to Calc Tip and Grand Total

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -12,7 +11,7 @@ import java.text.NumberFormat
 class MainActivity : AppCompatActivity() {
 
     // lateinit initializes variable before use
-    lateinit var tipCalc: ActivityMainBinding
+    private lateinit var tipCalc: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,35 +25,41 @@ class MainActivity : AppCompatActivity() {
         //startActivity(Intent(this, MainActivity::class.java)) //Just restarts app, not reset view
         tipCalc.totalAmount.text = null
         tipCalc.taxAmount.text = null
-        tipCalc.totalDisplayed.text = "$0.00"
-        tipCalc.tipDisplayed.text = "$0.00"
+        tipCalc.totalDisplayed.text = getString(R.string.grand_total_displayed)
+        tipCalc.tipDisplayed.text = getString(R.string.tip_amount_displayed)
         tipCalc.tipOptions.clearCheck()
         tipCalc.tipOptions.check(R.id.zero_percent)
     }
 
     private fun tipCalculator() {
+        if(!checkInput()){return}
         val percent = when (tipCalc.tipOptions.checkedRadioButtonId) {
             R.id.five_percent -> 0.05
             R.id.ten_percent -> 0.10
             R.id.twenty_percent -> 0.20
             else -> 0.00
         }
-        var totalAmount = 0.00
-        if (!tipCalc.totalAmount.text.isBlank()){
-            totalAmount = tipCalc.totalAmount.text.toString().toDouble()
-        }
-        var taxAmount = 0.00
-        if(!tipCalc.taxAmount.text.isBlank()){
-            taxAmount = tipCalc.taxAmount.text.toString().toDouble()
-        }
+        val totalAmount = tipCalc.totalAmount.text.toString().toDouble()
+        val taxAmount = tipCalc.taxAmount.text.toString().toDouble()
         var tempTip = totalAmount * percent
         val df = DecimalFormat("#.##")
         df.roundingMode = RoundingMode.HALF_UP
         tempTip = df.format(tempTip).toDouble()
-
         tipCalc.tipDisplayed.text = NumberFormat.getCurrencyInstance().format(tempTip).toString()
         val grandTotal = df.format(totalAmount + taxAmount + tempTip).toDouble()
         tipCalc.totalDisplayed.text = NumberFormat.getCurrencyInstance().format(grandTotal).toString()
+    }
+
+    private fun checkInput(): Boolean{
+        if (tipCalc.totalAmount.text.isBlank()){
+            tipCalc.totalAmount.error = "This field is required"
+            return false
+        }
+        if(tipCalc.taxAmount.text.isBlank()){
+            tipCalc.taxAmount.error = "This field is required"
+            return false
+        }
+        return true
     }
 
 
